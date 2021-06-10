bca1d12430dcfa886ab429a099ff24fdd521ead1
lynard <lynardsalingujay@outlook.com>
2021-06-10 22:51:52 +1200

    added first vue project

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Hero from '../components/Hero';
 // import Carousel from '../components/Carousel';
+import Content from '../components/Content';
 
 function HomePage(props) {
   return (
@@ -8,8 +9,20 @@ function HomePage(props) {
       <Hero
         title='Dev / Student'
         subTitle='Based in Auckland'
-        text='Skills and Interests: Python, Java, React, Docker, Cybersecurity'
+        text='Skills and Interests: Python, Java, React, Vue, Docker, Cybersecurity'
       />
+      <Content>
+        <p>
+          Vue Project #1:{' '}
+          <a
+            href='https://lynardsalingujay-vue-profile-ran-gen.netlify.app/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            Generate Random Profile
+          </a>
+        </p>
+      </Content>
     </div>
   );
 }
